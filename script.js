6440bf4531f05ad12dd65781b8242761584622f2
Mehedi <mehedi.hasan.tsx@gmail.com>
2026-01-27 13:35:57 +0600

    Fixed remove fuction

---
+++ script.js
@@ -0,0 +1,47 @@
+function rmvDevLens() {
+    document.getElementById("DevLens").style.display = "none";
+}
+
+function rmvStyleSpy() {
+    document.getElementById("StyleSpy").style.display = "none";
+}
+
+function rmvSpeedBoost() {
+    document.getElementById("SpeedBoost").style.display = "none";
+}
+
+function rmvJSONWizard() {
+    document.getElementById("JSONWizard").style.display = "none"
+}
+
+function rmvTabMasterPro() {
+    document.getElementById("TabMasterPro").style.display = "none";
+}
+
+function rmvViewportBuddy() {
+    document.getElementById("ViewportBuddy").style.display = "none";
+}
+
+function rmvMarkupNotes() {
+    document.getElementById("MarkupNotes").style.display = "none";
+}
+
+function rmvGridGuides() {
+    document.getElementById("GridGuides").style.display = "none";
+}
+
+function rmvPalettePicker() {
+    document.getElementById("PalettePicker").style.display = "none";
+}
+
+function rmvLinkChecker() {
+    document.getElementById("LinkChecker").style.display = "none";
+}
+
+function rmvDOMSnapshot() {
+    document.getElementById("DOMSnapshot").style.display = "none";
+}
+
+function rmvConsolePlus() {
+    document.getElementById("ConsolePlus").style.display = "none";
+}
